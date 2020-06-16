9b4014f2cff9c66c48967857eb097e86a6f8df2c
Dongwan Kim <dongwan.don.kim@gmail.com>
2020-06-16 00:42:51 -0700

    git accout change test

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   state = { term: '' };
-
   onFormSubmit = (event) => {
     event.preventDefault();
     this.props.onSubmit(this.state.term);
